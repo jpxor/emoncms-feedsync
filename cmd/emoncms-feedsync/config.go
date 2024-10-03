737a325651f5459421b84e028bb8a915da0377bb
Josh <jsimonot@live.ca>
2024-10-02 20:46:21 -0400

    add example config file and config struct/loader

---
+++ cmd/emoncms-feedsync/config.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type ServerInfo struct {
+	Host   string `yaml:"host"`
+	APIKey string `yaml:"apikey"`
+}
+
+type Config struct {
+	Feeds    []string   `yaml:"feeds"`
+	Local    ServerInfo `yaml:"local"`
+	Remote   ServerInfo `yaml:"remote"`
+	Interval int        `yaml:"interval"`
+}
+
+func readConfig(filename string) (*Config, error) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	var config Config
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		return nil, err
+	}
+	return &config, nil
+}
